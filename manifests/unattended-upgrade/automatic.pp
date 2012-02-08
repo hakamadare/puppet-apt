519e3e36b6a0940b223156595583f7aa9b71ea92
Steve Huff <shuff@vecna.org>
2012-02-08 11:44:50 -0500

    enable ensure parameter
    on apt::unattended-upgrade::automatic

--- manifests/unattended-upgrade/automatic.pp
+++ manifests/unattended-upgrade/automatic.pp
@@ -5,14 +5,19 @@ class apt::unattended-upgrade::automatic inherits apt::unattended-upgrade {
     content => "APT::Periodic::Unattended-Upgrade \"1\";\n",
   }
 
-  $dist = $::lsbdistid? {
-    Debian  => $::lsbdistcodename,
-    default => $::lsbdistid,
-  }
-
-  apt::conf{'50unattended-upgrades':
-    ensure  => present,
-    content => template("apt/unattended-upgrades.${dist}.erb"),
+  case $lsbdistid {
+    'Debian': {
+      apt::conf{'50unattended-upgrades':
+        ensure  => $ensure,
+        content => template("apt/unattended-upgrades.${lsbdistcodename}.erb"),
+      }
+    }
+    'Ubuntu': {
+      apt::conf{'50unattended-upgrades':
+        ensure  => $ensure,
+        content => template("apt/unattended-upgrades.${lsbdistid}.erb"),
+      }
+    }
   }
 
 }
